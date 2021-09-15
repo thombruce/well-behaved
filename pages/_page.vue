b62bd278e919f82ea23aaa76cee89c9f3c3eb80d
Thom Bruce <thom@thombruce.com>
2021-09-15 17:57:05 +0100

    Nuxt TNT default pages

---
+++ pages/_page.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+div
+  TntContent(v-if='!Array.isArray(page)' :article='page')
+  article(v-else)
+    header
+      h1 {{ slug | titleize }}
+    TntBlogList(:articles='page')
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, $taxonomies, params }) {
+    const slug = params.page
+
+    const page = await $content(slug)
+      .where({ draft: { $ne: true } })
+      .sortBy('date', 'desc')
+      .fetch()
+      .catch(async () => {
+        const terms = await $taxonomies(slug, '', { deep: true }).all()
+        return terms
+      })
+
+    return { slug, page }
+  }
+}
+</script>
